b778c265a6323ccaffb603018bdcded58e57cb48
hatateaya <amcjedhfeh@gmail.com>
2025-11-12 05:33:33 +0800

    add README

---
+++ README.sh
@@ -0,0 +1,7 @@
+# Scripts
+
+## install_teleirc.sh
+
+To Use:
+
+`wget https://raw.githubusercontent.com/hatateaya/scripts/refs/heads/main/install_teleirc.sh -O install_teleirc.sh && chmod +x install_teleirc.sh && ./install_teleirc.sh && rm install_teleirc.sh`
