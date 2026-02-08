8ec22f48dd5ba0ff7619eb58f47148da66e6dec4
hatateaya <amcjedhfeh@gmail.com>
2026-02-08 12:57:44 +0800

    let mksquashfs use zstd algorithm

--- make_luks_squashfs.sh
+++ make_luks_squashfs.sh
@@ -3,6 +3,6 @@
 read -p "Please enter your File Directory:" directory
 read -p "Please enter your Backup Filename:" filename
 
-sudo mksquashfs "${directory}" "${filename}"
+sudo mksquashfs "${directory}" "${filename}" -comp zstd
 sudo truncate -s +8M "${filename}"
 sudo cryptsetup reencrypt --type luks2 --resilience none --disable-locks --reduce-device-size 8M --encrypt "${filename}"
